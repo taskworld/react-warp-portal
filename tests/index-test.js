1ec233aad84458d605f860a1bd067156d2afa4d5
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-08-23 14:06:05 +0700

    Add more tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -25,6 +25,7 @@ describe('createWarp', () => {
         <div className='a'>
           <WarpPortal content={<div className='b'></div>}><div className='e'></div></WarpPortal>
           <WarpPortal content={<div className='c'></div>}><div className='f'></div></WarpPortal>
+          <WarpPortal content={<div className='g'></div>} />
         </div>
         <div className='d'>
           <WarpDestination />
@@ -34,12 +35,13 @@ describe('createWarp', () => {
     render(element, node, () => {
       setTimeout(() => {
         try {
-          assert(node.querySelector('.d .b'), '.d .b must exist')
-          assert(node.querySelector('.d .c'), '.d .c must exist')
-          assert(!node.querySelector('.a .b'), '.a .b must not exist')
-          assert(!node.querySelector('.a .c'), '.a .c must not exist')
-          assert(node.querySelector('.a .e'), '.a .e must exist')
-          assert(node.querySelector('.a .f'), '.a .f must exist')
+          assertElement('.d .b')
+          assertElement('.d .c')
+          assertElement('.d .g')
+          assertNoElement('.a .b')
+          assertNoElement('.a .c')
+          assertElement('.a .e')
+          assertElement('.a .f')
           done()
         } catch (e) {
           console.log(node.innerHTML)
@@ -48,4 +50,75 @@ describe('createWarp', () => {
       }, 100)
     })
   })
+
+  it('renders component with state', (done) => {
+    let _app
+    const App = createApp()
+    const element = (
+      <div>
+        <App ref={(app) => (_app = app)} />
+        <WarpDestination />
+      </div>
+    )
+    render(element, node, () => {
+      setTimeout(() => {
+        try {
+          assertElement('.app .child .thing')
+          assertNoElement('.Warp')
+          assertNoElement('.counter')
+          _app.toggle()
+          assertElement('.Warp')
+          assert.equal(assertElement('.counter').getAttribute('data-value'), '1')
+          _app.update()
+          assert.equal(assertElement('.counter').getAttribute('data-value'), '2')
+          _app.toggle()
+          assertNoElement('.counter')
+          done()
+        } catch (e) {
+          console.log(node.innerHTML)
+          done(e)
+        }
+      }, 100)
+    })
+    function createApp () {
+      return class App extends React.Component {
+        constructor (props) {
+          super(props)
+          this.state = { open: false, counter: 1 }
+        }
+        toggle () {
+          this.setState({ open: !this.state.open })
+        }
+        update () {
+          this.setState({ counter: this.state.counter + 1 })
+        }
+        renderContent () {
+          if (!this.state.open) return null
+          return (
+            <div className='warped'>
+              <div className='counter' data-value={this.state.counter}></div>
+            </div>
+          )
+        }
+        render () {
+          return (
+            <div className='app'>
+              <WarpPortal content={this.renderContent()}>
+                <div className='child'><span className='thing'>yep!</span></div>
+              </WarpPortal>
+            </div>
+          )
+        }
+      }
+    }
+  })
+
+  function assertElement (selector) {
+    const el = node.querySelector(selector)
+    assert(el, selector + ' must exist')
+    return el
+  }
+  function assertNoElement (selector) {
+    assert(!node.querySelector(selector), selector + ' must not exist')
+  }
 })
