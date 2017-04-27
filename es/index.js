2d029c1f83e8a124a991127cf01589ec0eee09e7
Chakrit <chakrit.lj@gmail.com>
2017-04-27 14:42:49 +0700

    Remove build folder

--- es/index.js
+++
@@ -1,4 +0,0 @@
-export { createWarp } from './createWarp';
-export { withWarpSourceBoundingClientRect } from './withWarpSourceBoundingClientRect';
-export { popup } from './popup';
-export { withWarpPortal } from './withWarpPortal';
